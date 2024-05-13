d9a100ac926568916c839ee11b037e6d8cdc20cd
Boris Antunez <antunez.boris@gmail.com>
2024-05-13 16:24:35 -0400

    Proyecto sonido

---
+++ main.js
@@ -0,0 +1,35 @@
+function playSonido (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+    
+  }
+  
+  const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+  for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio)
+
+    tecla.onclick = function (){
+        playSonido(idAudio);
+    };
+
+    tecla.onkeydown = function (){
+        tecla.classList.add('activa');
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('activa');
+    }
+    tecla.onblur = function() {
+        tecla.classList.remove('activa');
+    }
+
+
+  }
+
+  
+  
